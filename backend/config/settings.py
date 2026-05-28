52dc49673d977cea8a41c47b6c82534016113508
manjunath9446 <manju.r.k9446@gmail.com>
2026-05-28 19:02:29 +0530

    setting.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,10 +79,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
