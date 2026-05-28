5d1a2914cc2a7cc8f4ef239abf25fb2c6da8aacf
manjunath9446 <manju.r.k9446@gmail.com>
2026-05-28 22:37:03 +0530

    added vite url__

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://breathe-esg-1-v1zk.onrender.com",
+    "https://breathe-esg-1-v1zk.onrender.com/api",
 });
 
 export default api;
